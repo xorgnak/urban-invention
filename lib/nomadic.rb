dca00b4f26a823fa042445dcde472f3d780b596f
pi <pi@vm>
2021-01-18 04:48:48 -0500

    meta

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -124,6 +124,8 @@ module Nomadic
       code { border: thin solid black;  padding: 0 1% 0 1%; }
       .action { border: thin dotted red; border-radius: 10px; }
     </style>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
