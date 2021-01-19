0a566692114a9e2f590dec516620fb20df814d8a
pi <pi@vm>
2021-01-19 18:27:15 -0500

    39

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -67,7 +67,7 @@ module Nomadic
       %[<textarea name='settings' style='width: 100%; height: 100%;'>#{x}</textarea>];
     end
     def edit
-      %[<textarea name='editor' style='width: 100%; height: 100%;'>#{self.md.value || INDEX}</textarea>];
+      %[<textarea name='editor' class='form' style='width: 100%; height: 100%;'>#{self.md.value || INDEX}</textarea>];
     end
     def wall;
       w = self.wal.to_a.reverse.map { |e|
