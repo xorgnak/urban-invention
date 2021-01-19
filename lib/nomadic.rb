0b36b416a714e7337f289279e34ff0239bb6df2f
pi <pi@vm>
2021-01-19 09:12:53 -0500

    39

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -50,7 +50,7 @@ module Nomadic
     def welcome; WELCOME; end
     def settings; %[<textarea name='settings' style='width: 100%; height: 100%;'><%= self.logs %></textarea>]; end
     def logs
-      self.log.reverse.map { |e| %[#{e}\n] }.join('')
+      self.log.to_a.reverse.map { |e| %[#{e}\n] }.join('')
     end
     def tasks *t
       prompt '[ ] '
