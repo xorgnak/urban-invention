3f53c20f46d623f283415761b315deda2f564cb0
pi <pi@vm>
2021-01-19 07:01:48 -0500

    39

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -158,6 +158,7 @@ module Nomadic
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
 	    </head>
 	    <body style='height: 100%; width: 100%; margin: 0; padding: 0;'>
 	    <form id='form' style='margin: 0, padding: 0;'>
@@ -209,7 +210,7 @@ module Nomadic
 		console.log("got", dd);
 		d.time = dd.timestamp;
 		$("#input").html(dd.cmd); 
-		$("#output").html(dd.output);
+		$("#output").html(marked(dd.output));
 		$("#form")[0].reset();
 		if ( $("#b").val() == '' ) {
                     $("#b").val(dd.input);
