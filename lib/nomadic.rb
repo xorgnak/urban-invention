47bfe202b4f4e07c0a75a2fbd99e50154067b7b9
pi <pi@vm>
2021-01-19 06:50:49 -0500

    39

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -129,7 +129,7 @@ module Nomadic
         rescue => re
           o = re
         end
-        self.log << "# cmd: #{h[:form][:do]}(#{ar})\n> #{Time.now.utc.to_s}"
+        self.log << "# cmd: #{h[:trigger]}(#{ar})\n> #{Time.now.utc.to_s}"
       end
       db[:stat] = self.stat.members(with_scores: true).to_h
       db[:attr] = self.attr.all
