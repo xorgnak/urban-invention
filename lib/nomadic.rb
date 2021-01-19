e542ee8fcead39c55bea22bc6c79e469dbd0f903
pi <pi@vm>
2021-01-18 21:29:01 -0500

    39

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -108,7 +108,6 @@ module Nomadic
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body style='height: 100%; width: 100%; margin: 0; padding: 0;'>
@@ -134,7 +133,7 @@ module Nomadic
 	    <script>
 	    // get unique id OR use one passed in.
 	var id = "<%= params[:id] || rand_id %>";
-        var state = { stage: 0 };
+        var db = { };
 	// turn form into json object.
 	function getForm() {
 	    var ia = {};
@@ -145,17 +144,18 @@ module Nomadic
             var d = { id: id, trigger: th, form: getForm() };
             ws.send(JSON.stringify(d));
 	}
+
+
 	$(function() {
-	    // create the mqtt client.
-            ws = new WebSocket('ws://vango.me');
-	    
-	    // set callback handlers
-	    ws.onopen = function() { console.log("open") };
-	    ws.onclose = function() { console.log("closed"); };
-	    ws.onmessage = function(m) {
-		console.log("onmessage", m);
-	    };
-	    
+	    var show = function(el){
+		return function(msg){ el.innerHTML = msg; }
+	    }(document.getElementById(''));
+
+	    var ws       = new WebSocket('ws://vango.me');
+	    ws.onopen    = function()  { show('websocket opened'); };
+	    ws.onclose   = function()  { show('websocket closed'); }
+	    ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
+	    	
 	    $(document).on('submit', "form", function(ev) { ev.preventDefault(); $("#exe").click(); });
 	    $(document).on('click', ".task", function(ev) { 
 		ev.preventDefault(); 
@@ -164,7 +164,6 @@ module Nomadic
 	    $(document).on('click', '#add', function(ev) {
 		ev.preventDefault();
 		$("#adds").toggle();
-		
 	    });
 	    $(document).on('click', '.do', function(ev) { 
 		ev.preventDefault(); 
