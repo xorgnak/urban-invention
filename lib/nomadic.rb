30dd407baa579375c0832ace527e6844a6fcaf08
pi <pi@vm>
2021-01-20 20:04:17 -0500

    74

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -29,7 +29,8 @@ if [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then
 export PM='apt -y';
 elif [ "$SYS" == "fedora" ]; then
 export PM='yum -y';
-else; fi
+else echo "no release ID." && exit
+ fi
 echo "PM: $PM"; sudo $PM update && sudo $PM upgrade sudo $PM install git
 git clone https://github.com/xorgnak/turbo-rotary-phone && mv turbo-rotary-phone pmm
 ]
