36f5d8878d2f10356856731fff37de594b645e4c
pi <pi@vm>
2021-01-20 20:00:02 -0500

    74

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -25,20 +25,9 @@ module Nomadic
   ##### FIND THIS A NEW HOME!
   PMM = %[
 source /etc/os-release
-if [ -z "$NAME" ]; then
-    export SYS=$NAME;
-else
-    export SYS=$ID;
-fi
-if [ "$SYS" == "debian" ] || [ "$SYS" == "ubuntu" ]; then
-    export PM='apt -y';
-elif [ "$SYS" == "fedora" ]; then
-    export PM='yum -y';
-fi
-echo "PM: $PM"
-sudo $PM update
-sudo $PM upgrade
-sudo $PM install git
+if [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then; export PM='apt -y';
+elif [ "$SYS" == "fedora" ]; then; export PM='yum -y';
+else; fi; echo "PM: $PM"; sudo $PM update && sudo $PM upgrade sudo $PM install git
 git clone https://github.com/xorgnak/turbo-rotary-phone && mv turbo-rotary-phone pmm
 ]
   #####
