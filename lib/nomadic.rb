520840bf588bd45c63c7ffb14a365b56ef18cdeb
pi <pi@vm>
2021-02-05 06:01:32 -0500

    74

--- lib/nomadic.rb
+++ lib/nomadic.rb
@@ -484,6 +484,53 @@ cd pmm && chmod +x install.sh && ./install.sh
 var d = { init: true, id: "<%= rand_id %>", pipe: "<%= rand_pipe %>" };
 var z;
 
+
+
+
+
+
+										     
+
+function btsearch() {
+    let filters = [];
+    
+    let filterService = document.querySelector('#service').value;
+    if (filterService.startsWith('0x')) { filterService = parseInt(filterService); }
+    if (filterService) { filters.push({services: [filterService]}); }
+    let filterName = document.querySelector('#name').value;
+    if (filterName) { filters.push({name: filterName}); }
+    let filterNamePrefix = document.querySelector('#namePrefix').value;
+    if (filterNamePrefix) { filters.push({namePrefix: filterNamePrefix}); }
+    let options = {};
+    if (document.querySelector('#allDevices').checked) { options.acceptAllDevices = true;
+						       } else { options.filters = filters; }
+    console.log('Requesting Bluetooth Device...');
+    console.log('with ' + JSON.stringify(options));
+    navigator.bluetooth.requestDevice(options)
+	.then(device => { console.log('> Name:             ' + device.name);
+			  console,log('> Id:               ' + device.id);
+			  console.log('> Connected:        ' + device.gatt.connected);
+			})
+	.catch(error => { console.log('Argh! ' + error); });
+}
+										     
+										     
+										     
+
+
+
+
+
+
+
+
+
+
+
+
+										     
+
+										     
 var ws  = new WebSocket('wss://' + window.location.host + '?pipe=' + d.pipe );                                                 
 ws.onopen = function() { message(d); console.log("WS: OPEN"); }                                                               
 ws.onclose = function() { console.log("WS: CLOSE"); }                                                                         
@@ -618,76 +665,6 @@ $(function() {
 	ev.preventDefault(); 
 	sendForm($(this).attr('id'));
     });
-
-///// END OF NOMADIC
-
-function btsearch() {
-    let filters = [];
-    
-    let filterService = document.querySelector('#service').value;
-    if (filterService.startsWith('0x')) {
-	filterService = parseInt(filterService);
-    }
-    if (filterService) {
-	filters.push({services: [filterService]});
-    }
-    
-    let filterName = document.querySelector('#name').value;
-    if (filterName) {
-	filters.push({name: filterName});
-    }
-    
-    let filterNamePrefix = document.querySelector('#namePrefix').value;
-    if (filterNamePrefix) {
-	filters.push({namePrefix: filterNamePrefix});
-    }
-    
-    let options = {};
-    if (document.querySelector('#allDevices').checked) {
-	options.acceptAllDevices = true;
-    } else {
-	options.filters = filters;
-    }
-    
-    log('Requesting Bluetooth Device...');
-    log('with ' + JSON.stringify(options));
-    navigator.bluetooth.requestDevice(options)
-	.then(device => {
-	    log('> Name:             ' + device.name);
-	    log('> Id:               ' + device.id);
-	    log('> Connected:        ' + device.gatt.connected);
-	})
-	.catch(error => {
-	    log('Argh! ' + error);
-	});
-}
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
 									     </script>										     </body>
